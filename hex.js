6d8bc886860d60d4d55c2d03dacd75d966135f92
Raunak Gayen <rnk.express@gmail.com>
2021-10-06 01:50:13 +0530

    Added index.htm syle.css hex.js

---
+++ hex.js
@@ -0,0 +1,9 @@
+const hex = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("bg_button"); // for the button
+const color = document.getElementById("bg_info"); // to show the color info
+btn.addEventListener('click',function () {
+    let cl = "#";
+    cl = cl + (hex[Math.floor(Math.random()*hex.length)]) + (hex[Math.floor(Math.random()*hex.length)]) + (hex[Math.floor(Math.random()*hex.length)]) + (hex[Math.floor(Math.random()*hex.length)]) + (hex[Math.floor(Math.random()*hex.length)]) + (hex[Math.floor(Math.random()*hex.length)]);
+    document.body.style.backgroundColor = cl;
+    color.innerHTML = cl;
+})
